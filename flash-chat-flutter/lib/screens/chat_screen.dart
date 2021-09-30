d044ee125e003f3b852f77a98c592133b3ef20e6
Aman Tiwari <amantiwariald18@gmail.com>
2021-09-30 08:55:53 +0530

    up

---
+++ flash-chat-flutter/lib/screens/chat_screen.dart
@@ -0,0 +1,110 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'welcome_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flash_chat/constants.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatScreen extends StatefulWidget {
+  static const String id = 'chat_screen';
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final Stream<QuerySnapshot> message =
+      FirebaseFirestore.instance.collection('messages').snapshots();
+  final _auth = FirebaseAuth.instance;
+  User loggedinUser;
+  String messageText;
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUser();
+  }
+
+  void getCurrentUser() async {
+    try {
+      final user = _auth.currentUser;
+      if (user != null) {
+        loggedinUser = user;
+        print(loggedinUser.email);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: null,
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.close),
+              onPressed: () {
+                _auth.signOut();
+                Navigator.pushNamed(context, WelcomeScreen.id);
+              }),
+        ],
+        title: Text('⚡️Chat'),
+        backgroundColor: Colors.lightBlueAccent,
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            StreamBuilder<QuerySnapshot>(
+                stream: message,
+                // ignore: missing_return
+                builder: (
+                  BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapShot,
+                ) {
+                  if (snapShot.hasData) {
+                    return Text('something ggdf');
+                  }
+                  if (snapShot.connectionState == ConnectionState.waiting) {
+                    return Text('Loading..');
+                  }
+                  final data = snapShot.requireData;
+
+                  return ListView.builder(
+                      itemCount: data.size,
+                      itemBuilder: (context, index) {
+                        return Text(
+                            '${data.docs[index]['Text']}  and ${data.docs[index]['Sender']}');
+                      });
+                }),
+            Container(
+              decoration: kMessageContainerDecoration,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      onChanged: (value) {
+                        messageText = value;
+                      },
+                      decoration: kMessageTextFieldDecoration,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Send',
+                      style: kSendButtonTextStyle,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
